472afca7386e4de42e47575dbeb135f045cafecd
Matthew <64306058+matthew-lc-zheng@users.noreply.github.com>
2021-05-21 14:30:37 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include "nt.hpp"
+
+using namespace std;
+
+main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  
+  cout << GCD(72,36) << endl;
+  cout << LCM(3,4) << endl;
+  cout << CRT({3, 5, 7}, {2, 3, 2}) << endl;
+}
