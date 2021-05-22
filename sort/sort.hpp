01e02051c0876c75829eb11f5c7b5e9991e172c7
Matthew <64306058+matthew-lc-zheng@users.noreply.github.com>
2021-05-22 11:11:28 +0800

    Update sort.hpp

--- sort/sort.hpp
+++ sort/sort.hpp
@@ -1,15 +1,14 @@
 #include <bits/stdc++.h>
 
-template<class T>
-void merge_sort(std::vector<T> &v) {
-  auto s = v.size();
-  if (s == 2 && v[0] > v[1])
-    std::swap(v[0], v[1]);
-  else if (s > 2) {
-    auto v1 = std::vector(v.begin(), v.begin() + (s >> 1));
-    auto v2 = std::vector(v.begin() + (s >> 1), v.end());
-    merge_sort(v1);
-    merge_sort(v2);
-    std::merge(v1.begin(), v1.end(), v2.begin(), v2.end(), v.begin());
+
+
+
+void quick_sort(std::vector<int>::iterator l, std::vector<int>::iterator r) {
+  if (l < r) {
+    srand(time(nullptr));
+    std::swap(*(l + rand() % (r - l)), *(r - 1));
+    auto p = std::stable_partition(l, r, [r](int i) { return i <= *(r - 1); });
+    quick_sort(l, p - 1);
+    quick_sort(p, r);
   }
 }
