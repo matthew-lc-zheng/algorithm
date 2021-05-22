1578fecfb82357e788a9825a1249162e8a0b4e23
Matthew <64306058+matthew-lc-zheng@users.noreply.github.com>
2021-05-22 11:20:06 +0800

    Update sort.hpp

--- sort/sort.hpp
+++ sort/sort.hpp
@@ -1,8 +1,30 @@
 #include <bits/stdc++.h>
 
+///
+/// \brief merge_sort
+/// \param l
+/// \param r
+/// \caption
+///
+void merge_sort(std::vector<int>::iterator l, std::vector<int>::iterator r) {
+  auto s = r - l;
+  if (s == 2 && *l > *(l + 1))
+    std::swap(*l, *(l + 1));
+  else if (s > 2) {
+    auto v1 = std::vector(l, l + (s >> 1));
+    auto v2 = std::vector(l + (s >> 1), r);
+    merge_sort(v1.begin(), v1.end());
+    merge_sort(v2.begin(), v2.end());
+    std::merge(v1.begin(), v1.end(), v2.begin(), v2.end(), l);
+  }
+}
 
-
-
+///
+/// \brief quick_sort
+/// \param l
+/// \param r
+/// \caption
+///
 void quick_sort(std::vector<int>::iterator l, std::vector<int>::iterator r) {
   if (l < r) {
     srand(time(nullptr));
