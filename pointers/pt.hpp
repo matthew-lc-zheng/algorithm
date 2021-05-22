e653c89466e086a8bd9c1a2c791082380aa35d96
Matthew <64306058+matthew-lc-zheng@users.noreply.github.com>
2021-05-22 17:28:52 +0800

    Update pt.hpp

--- pointers/pt.hpp
+++ pointers/pt.hpp
@@ -1,26 +1,27 @@
 #ifndef PT_HPP
 #define PT_HPP
 
+///
+/// \brief MSA: Maximum sum of subarray
+/// \param l
+/// \param r
+/// \return
+/// \caption 
+///
 int MSA(std::vector<int>::iterator l, std::vector<int>::iterator r) {
   auto maxv = *max_element(l, r);
   if (maxv < 0)
     return maxv;
-  int sum, maxs;
-  sum = maxs = 0;
+  int sum=0, maxs=0;  
   auto p = l;
   while (p < r) {
-    if (*p > 0)
-      sum += *p;
-    else if (sum + *p > 0)
-      sum += *p;
-    else
-      sum = 0;
+    sum = *p > 0 || sum + *p > 0 ? *p + sum : 0;
     maxs = std::max(maxs, sum);
     ++p;
   }
   return maxs;
 }
-
+//----------------------------------------------------------------------------------
 
 
 #endif
